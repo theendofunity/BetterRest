bb862d682729e9473aef08950626764218c59f0f
Dmitriy Dudkin <d.dudkin@growapp.biz>
2023-04-20 00:19:37 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Дмитрий Дудкин on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
